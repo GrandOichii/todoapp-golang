0dc1a396a8f9ff64ff4ac6139e79ba02bd98fc23
Igor Yastrebov <i.hawk@yandex.ru>
2024-05-16 03:50:08 -0400

    Add basic implementation of user service

--- api/dto/user/PostUser.go
+++ api/dto/user/PostUser.go
@@ -1,6 +1,16 @@
 package dto
 
+import "github.com/GrandOichii/todoapp-golang/api/models"
+
 type PostUser struct {
 	Username string `json:"username" validate:"required,gt=4,lt=20"`
-	Password string `json:"password" validate:"required,gt=8,lt=20"`
+	Password string `json:"password" validate:"required,gte=8,lt=20"`
+}
+
+func (u PostUser) ToUser() *models.User {
+	// TODO hash password
+	return &models.User{
+		Username:     u.Username,
+		PasswordHash: u.Password,
+	}
 }
--- api/services/user/user_service_impl.go
+++ api/services/user/user_service_impl.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"errors"
+	"fmt"
+
 	dto "github.com/GrandOichii/todoapp-golang/api/dto/user"
 	repositories "github.com/GrandOichii/todoapp-golang/api/repositories/user"
 	"github.com/go-playground/validator/v10"
@@ -21,11 +24,40 @@ func CreateUserServiceImpl(repo repositories.UserRepository, validate *validator
 }
 
 func (ser UserServiceImpl) Register(user *dto.PostUser) error {
-	// TODO
+	if err := ser.validate.Struct(user); err != nil {
+		return err
+	}
+
+	existing := ser.repo.FindByUsername(user.Username)
+	if existing != nil {
+		return fmt.Errorf("user with username %s already exists", user.Username)
+	}
+
+	newUser := user.ToUser()
+	ser.repo.Save(newUser)
+
 	return nil
 }
 
 func (ser UserServiceImpl) Login(user *dto.PostUser) (*LoginResult, error) {
-	// TODO
-	return nil, nil
+	if err := ser.validate.Struct(user); err != nil {
+		return nil, err
+	}
+
+	existing := ser.repo.FindByUsername(user.Username)
+	if existing == nil {
+		return nil, errors.New("incorrect username or password")
+	}
+
+	// TODO check hash
+	if existing.PasswordHash != user.Password {
+		return nil, errors.New("incorrect username or password")
+	}
+
+	// TODO generate token
+	token := "jwt token"
+
+	return &LoginResult{
+		Token: token,
+	}, nil
 }
