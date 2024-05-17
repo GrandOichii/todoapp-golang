e2cae95c543db9061ea8abddc2216c11cb25773d
Igor Yastrebov <i.hawk@yandex.ru>
2024-05-17 04:33:17 -0400

    Add back old config structs

--- api/config/config.go
+++ api/config/config.go
@@ -8,34 +8,20 @@ import (
 	"github.com/sethvargo/go-envconfig"
 )
 
-// type CollectionConfiguration struct {
-// 	Name string `json:"name"`
-// }
+type CollectionConfiguration struct {
+	Name string `json:"name" env:"NAME,required"`
+}
 
-// type DbConfiguration struct {
-// 	ConnectionUri  string                  `json:"connectionUri"`
-// 	DbName         string                  `json:"dbName" env:"NAME,required"`
-// 	TaskCollection CollectionConfiguration `json:"taskCollection"`
-// 	UserCollection CollectionConfiguration `json:"userCollection"`
-// }
-
-// type Configuration struct {
-// 	Port string          `json:"port" env:"PORT,required"`
-// 	Db   DbConfiguration `json:"db" env:"DB,required"`
-// }
+type DbConfiguration struct {
+	ConnectionUri  string                  `json:"connectionUri" env:"CONNECTION_URI,required"`
+	DbName         string                  `json:"dbName" env:"NAME,required"`
+	TaskCollection CollectionConfiguration `json:"taskCollection" env:",required,prefix=TASK_COLLECTION_"`
+	UserCollection CollectionConfiguration `json:"userCollection" env:",required,prefix=USER_COLLECTION_"`
+}
 
 type Configuration struct {
-	Port string `json:"port" env:"PORT,required"`
-	Db   struct {
-		ConnectionUri  string `json:"connectionUri" env:"DB_CONNECTION_URI,required"`
-		DbName         string `json:"dbName" env:"DB_NAME,required"`
-		TaskCollection struct {
-			Name string `json:"name" env:"DB_TASK_COLLECTION_NAME,required"`
-		} `json:"taskCollection"`
-		UserCollection struct {
-			Name string `json:"name" env:"DB_USER_COLLECTION_NAME,required"`
-		} `json:"userCollection"`
-	} `json:"db"`
+	Port string          `json:"port" env:"PORT,required"`
+	Db   DbConfiguration `json:"db" env:",required,prefix=DB_"`
 }
 
 func ReadConfig(path string) (*Configuration, error) {
